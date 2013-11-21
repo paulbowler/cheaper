c57dc53fcc41ae220c4ed117219aac94b73ad439
Paul Bowler <paul_bowler@yahoo.com>
2013-11-21 12:28:27 +0000

    First ideas

--- src/cheaper/core.clj
+++ src/cheaper/core.clj
@@ -1,6 +1,15 @@
-(ns cheaper.core)
+(ns cheaper.core
+   (:require [net.cgrand.enlive-html :as html]
+             [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:dynamic *base-url* "http://www.amazon.co.uk/Doctor-Character-Building-Construction-Playset/dp/B004W1T8YW/ref=pd_ys_sf_s_468292_b6_3_p?ie=UTF8&refRID=1W16R8191GXJJSX6KGF5")
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn price []
+  (map html/text (html/select (fetch-url *base-url*) [:b.priceLarge html/text])))
+
+(price)
+
+(resolve (symbol "cheaper.core/price"))
